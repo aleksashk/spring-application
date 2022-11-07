b4e1b2e9de8b1a72965a68ac9d0a30d36c152b92
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-07 15:48:20 +0300

    @Autowired @Component

--- src/main/java/ru/philimonov/spring/Starter.java
+++ src/main/java/ru/philimonov/spring/Starter.java
@@ -6,13 +6,8 @@ public class Starter {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
 
-            Music music = context.getBean("classicalMusic", Music.class);
-            Music rockMusic = context.getBean("rockMusic", Music.class);
-
-            MusicPlayer musicPlayer = new MusicPlayer(music);
-            MusicPlayer rockMusicPlayer = new MusicPlayer(rockMusic);
-            rockMusicPlayer.playMusic();
-            musicPlayer.playMusic();
+            MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
+            player.playMusic();
         }
     }
 }
