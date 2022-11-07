dcb96f127140eef1d0ca3dab8148c398376b3029
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-07 16:06:17 +0300

    @Autowired @Qualifier

--- src/main/java/ru/philimonov/spring/MusicPlayer.java
+++ src/main/java/ru/philimonov/spring/MusicPlayer.java
@@ -1,11 +1,13 @@
 package ru.philimonov.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
     @Autowired
+    @Qualifier("rockMusic")
     private Music music;
 
 
