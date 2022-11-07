dc5c01b9876623cc597bbe40c6d9d279c1e4bbc4
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-07 16:05:27 +0300

    @Autowired two beans is actual

--- src/main/java/ru/philimonov/spring/MusicPlayer.java
+++ src/main/java/ru/philimonov/spring/MusicPlayer.java
@@ -5,17 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+    private Music music;
+
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong() + ", " + rockMusic.getSong();
+        return "Playing: " + music.getSong();
     }
 }
