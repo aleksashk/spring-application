4e28295d01000a944985e37cf762244ef88bcb16
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-07 12:17:30 +0300

    DI with xml and setter

--- src/main/java/ru/philimonov/spring/FolkMusic.java
+++
@@ -1,8 +0,0 @@
-package ru.philimonov.spring;
-
-public class FolkMusic implements Music{
-    @Override
-    public String playMusic() {
-        return "Dirty Old Town";
-    }
-}
--- src/main/java/ru/philimonov/spring/MusicPlayer.java
+++ src/main/java/ru/philimonov/spring/MusicPlayer.java
@@ -1,9 +1,17 @@
 package ru.philimonov.spring;
 
 public class MusicPlayer {
+
     private Music music;
 
+    public MusicPlayer() {
+    }
+
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
