fd7e64f132a03298e68cf7244ec41f941c396e70
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-11-07 15:33:15 +0300

    @Component without id

--- src/main/java/ru/philimonov/spring/RockMusic.java
+++ src/main/java/ru/philimonov/spring/RockMusic.java
@@ -2,7 +2,7 @@ package ru.philimonov.spring;
 
 import org.springframework.stereotype.Component;
 
-@Component("musicBean")
+@Component
 public class RockMusic implements Music {
     @Override
     public String getSong() {
--- src/main/java/ru/philimonov/spring/Starter.java
+++ src/main/java/ru/philimonov/spring/Starter.java
@@ -6,7 +6,7 @@ public class Starter {
     public static void main(String[] args) {
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
 
-            Music music = context.getBean("musicBean", Music.class);
+            Music music = context.getBean("rockMusic", Music.class);
 
             MusicPlayer musicPlayer = new MusicPlayer(music);
 
